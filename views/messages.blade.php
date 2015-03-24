fd6b8eaeb09d19b92875621eed0768e7b8703dbc
Philip Hughes <phil@hughesbox.co.uk>
2015-03-24 11:11:44 +0000

    Add title to standard view template

--- views/messages.blade.php
+++ views/messages.blade.php
@@ -3,6 +3,10 @@
 		<div class="alert alert-{{ $toast['level'] }}">
 			<button type="button" class="close" data-dismiss="alert" aria-hidden="true">&times;</button>
 
+			@if(!is_null($toast['title']))
+				<strong>{{ $toast['title'] }}</strong>
+			@endif
+
 			{{ $toast['message'] }}
 		</div>
 	@endforeach
